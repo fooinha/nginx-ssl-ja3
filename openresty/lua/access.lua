ca5f54d3341a9c6a6ada98c889a159da1b9b2ad2
Andrea Sessa <andrea.sessa@cleafy.com>
2019-05-29 10:46:56 +0200

    Store also js3 numerical fingerprint

--- openresty/lua/access.lua
+++ openresty/lua/access.lua
@@ -2,5 +2,6 @@ local redis = require "redis"
 
 local hash = ngx.req.get_headers()["x-ja3-hash"]
 local ua = ngx.req.get_headers()["x-ja3-ua"]
+local fp = ngx.req.get_headers()["x-ja3-fp"]
 
-redis.RedisSet(hash, ua)
+redis.RedisSet(hash, ua..'/n'..fp)
